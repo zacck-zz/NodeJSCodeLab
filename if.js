0ac0e41abda3edad9c58d25f569040c04bbe3298
Zacck <coderv63@gmail.com>
2016-10-05 12:58:41 +0200

    work on if challenge

--- if.js
+++ if.js
@@ -1,7 +1,19 @@
-/jt var averageGrade = 98;
+var averageGrade = 98;
 
 if(averageGrade > 70) {
   console.log('You Passed the Course');
 } else {
   console.log('You did not pass the Course would you like to try again');
+}
+
+
+//challenge
+var friend = 'Keegan';
+
+if(friend === 'Michael' || friend === 'Marike' || friend === 'Steph' || friend === 'Keegan') {
+  console.log('Hey', friend);
+} else if (friend === 'Badane') {
+  console.log('I dont like you :(');
+} else {
+  console.log('Hey!');
 }
