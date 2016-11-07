12b6f161b92cc1709902d09814ae1481e41cece0
Zacck <coderv63@gmail.com>
2016-11-07 09:23:44 +0200

    work on loop samples for the app

---
+++ loops.js
@@ -0,0 +1,18 @@
+//iterator
+var i = 0;
+var countLimit = 9;
+
+//while loop
+while(i < countLimit) {
+  //lets log to screen
+  console.log('while', i);
+  //lets increment the i
+  i++;
+}
+
+
+//for loop
+for(i = 0; i < countLimit; i++) {
+  //lets log on every iteration
+  console.log('for', i);
+}
