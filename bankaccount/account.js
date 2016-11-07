0fd15b6f344433543620bc5afdc38931986269af
Zacck <coderv63@gmail.com>
2016-11-07 09:00:05 +0200

    working with models using bank accounts

--- bankaccount/account.js
+++ bankaccount/account.js
@@ -1,9 +1,38 @@
-var account =  {
+/*Account vars
   userName: null,
   balance: 3,
   lastTrans: null
-};
+*/
 
+//a set of accounts
+var currentAccounts = [];
+
+//add a func to create accounts
+function createAccount(username, balance) {
+  //use the data to make an account
+  var account = {
+    userName: username,
+    balance
+  }
+  //lets push the account into the sets of accounts
+  currentAccounts.push(account);
+  return account;
+}
+
+//function to get user account
+function getAccount(username) {
+  var UserAccount = undefined;
+  //find matching account
+  currentAccounts.forEach((account) => {
+    //check which username matches which account
+    if(username === account.userName) {
+      //return matching account
+      UserAccount = account
+    }
+  });
+
+  return UserAccount;
+}
 
 //withdraw
 function withdraw(account, amount) {
@@ -13,9 +42,9 @@ function withdraw(account, amount) {
       type: 'withdraw',
       amount: amount
     };
-    console.log('Withdrawal of', amount, 'Successful');
+    console.log('Withdrawal of', amount, 'from', account.userName, 'Successful');
   } else {
-    console.log('Balance of', account.balance, 'Cannot withdraw ', amount);
+    console.log('Balance of', account.balance, 'from', account.userName, 'Cannot withdraw ', amount);
   }
 };
 
@@ -27,9 +56,9 @@ function deposit(account, amount) {
       type: 'deposit',
       amount: amount
     };
-    console.log('deposit of', amount, 'done');
+    console.log('deposit of', amount, 'to', account.userName, 'done');
   } else {
-    console.log('deposit of', amount , 'not possible');
+    console.log('deposit of', amount , 'from', account.userName, 'not possible');
   }
 };
 
@@ -38,11 +67,13 @@ function getBalance(account) {
   console.log('current balance is', account.balance);
 };
 
-deposit(account, 80);
-getBalance(account);
 
-withdraw(account, 90);
-getBalance(account);
+var zacckAccount =  createAccount('zacck', 200);
+deposit(zacckAccount, 80);
+getBalance(zacckAccount);
+withdraw(zacckAccount, 90);
+getBalance(zacckAccount);
 
-withdraw(account, 10);
-getBalance(account);
+var minionAccount = createAccount('Minion', 30);
+withdraw(minionAccount, 10);
+getBalance(minionAccount);
