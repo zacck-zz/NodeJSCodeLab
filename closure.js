20e9e0684b27fdff77d2c1e9273abcb14d6d3872
zacck <coderv63@gmail.com>
2017-03-22 07:55:15 +0200

    work on closure sample

--- closure.js
+++ closure.js
@@ -19,3 +19,21 @@ mGreeter();
 *RETURN  a fucntion that takes a number to add
 *its gonna return the sum of the passed in number and the baseNumber
 */
+
+//prototype of our adder this takes the base number
+function createAdder(number) {
+  //inner function to do the actual addition
+  function adder(toAdd) {
+    console.log('sum is ', number + toAdd)
+  }
+
+  return adder;
+}
+
+//lets return our function with a base of 12
+var mAdder = createAdder(12);
+
+//lets run the adder with 12 again
+//should return 24
+//composable
+mAdder(12);
